a684cfe846696518a4459ae93bec7292dcd0d045
이승호\Lee <sh052300@naver.com>
2018-08-16 10:36:28 +0900

    Lee

---
+++ RacingGame/Assets/UI/script/MainUIScript.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainUIScript : MonoBehaviour {
+    public int NowNum = 1;
+    public int[] BtnNum = new int[3];
+    public GameObject[] Btnbar = new GameObject[4];
+    public GameObject After_P;
+    public GameObject[] PlayBtnbar = new GameObject[3];
+
+	// Use this for initialization
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        First_btn();
+        Debug.Log(NowNum);
+	}
+    public void First_btn()
+    {
+        if (NowNum == 1)
+        {
+            After_P.SetActive(false);
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                NowNum += 4;
+                After_P.SetActive(true);
+                
+
+            }
+
+            else if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                NowNum += 1;
+            }
+            Btnbar[0].SetActive(true);
+            Btnbar[1].SetActive(false);
+            Btnbar[2].SetActive(false);
+            Btnbar[3].SetActive(false);
+            PlayBtnbar[0].SetActive(false);
+            PlayBtnbar[1].SetActive(false);
+            PlayBtnbar[2].SetActive(false);
+
+        }
+        else if (NowNum == 2)
+        {
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                NowNum += 1;
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                NowNum -= 1;
+            }
+            Btnbar[0].SetActive(false);
+            Btnbar[1].SetActive(true);
+            Btnbar[2].SetActive(false);
+            Btnbar[3].SetActive(false);
+        }
+        else if (NowNum == 3)
+        {
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                NowNum += 1;
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                NowNum -= 1;
+            }
+            Btnbar[0].SetActive(false);
+            Btnbar[1].SetActive(false);
+            Btnbar[2].SetActive(true);
+            Btnbar[3].SetActive(false);
+        }
+        else if (NowNum == 4)
+        {
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                NowNum -= 1;
+            }
+            Btnbar[0].SetActive(false);
+            Btnbar[1].SetActive(false);
+            Btnbar[2].SetActive(false);
+            Btnbar[3].SetActive(true);
+        }
+        else if (NowNum == 5)
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                NowNum = 1;
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                NowNum += 1;
+            }
+            PlayBtnbar[0].SetActive(true);
+            PlayBtnbar[1].SetActive(false);
+            PlayBtnbar[2].SetActive(false);
+            Btnbar[0].SetActive(false);
+            Btnbar[1].SetActive(false);
+            Btnbar[2].SetActive(false);
+            Btnbar[3].SetActive(false);
+        }
+        else if(NowNum==6)
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                NowNum = 1;
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                NowNum += 1;
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                NowNum -= 1;
+            }
+            PlayBtnbar[0].SetActive(false);
+            PlayBtnbar[1].SetActive(true);
+            PlayBtnbar[2].SetActive(false);
+        }
+        else if(NowNum==7)
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                NowNum = 1;
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                NowNum -= 1;
+            }
+            PlayBtnbar[0].SetActive(false);
+            PlayBtnbar[1].SetActive(false);
+            PlayBtnbar[2].SetActive(true);
+
+        }
+            
+        }
+    }
---
+++ RacingGame/Assets/UI/script/option.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class option : MonoBehaviour {
+    public int NowNum=1;
+    public int NowNum_master = 1;
+    public int NowNum_music= 1;
+    public int NowNum_engine = 1;
+    public int NowNum_fx = 1;
+    public GameObject master;
+    public GameObject music;
+    public GameObject engine;
+    public GameObject FX;
+    public GameObject[] underline = new GameObject[4];
+    public GameObject[] master_num = new GameObject[10];
+    public GameObject[] music_num = new GameObject[10];
+    public GameObject[] engine_num = new GameObject[10];
+    public GameObject[] FX_num = new GameObject[10];
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        select_underline();
+        Debug.Log(NowNum);
+        master
+
+
+    }
+    public void select_underline()
+    {
+        if(NowNum==1)
+        {
+            underline[0].SetActive(true);
+            underline[1].SetActive(false);
+            underline[2].SetActive(false);
+            underline[3].SetActive(false);
+            if(Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+
+            }
+            else if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+
+            }
+            else if(Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                NowNum += 1;
+            }
+
+        }
+        else if (NowNum == 2)
+        {
+            underline[0].SetActive(false);
+            underline[1].SetActive(true);
+            underline[2].SetActive(false);
+            underline[3].SetActive(false);
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                NowNum += 1;
+            }
+            else if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                NowNum -= 1;
+            }
+
+        }
+        else if (NowNum == 3)
+        {
+            underline[0].SetActive(false);
+            underline[1].SetActive(false);
+            underline[2].SetActive(true);
+            underline[3].SetActive(false);
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                NowNum += 1;
+            }
+            else if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                NowNum -= 1;
+            }
+        }
+        else if (NowNum == 4)
+        {
+            underline[0].SetActive(false);
+            underline[1].SetActive(false);
+            underline[2].SetActive(false);
+            underline[3].SetActive(true);
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                NowNum -= 1;
+                
+            }
+        }
+    }
+}
